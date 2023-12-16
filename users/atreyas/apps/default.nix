ecffc0202a081e1c90a8c17f2a606168f4235035
Atreya Srivathsan <1410778+atreyas@users.noreply.github.com>
2023-12-16 09:35:09 -0800

    Setup nnn

--- users/atreyas/apps/default.nix
+++ users/atreyas/apps/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
-    ./neovim
     ./git
+    ./neovim
+    ./nnn
   ];
 }
---
+++ users/atreyas/apps/nnn/default.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, lib, ... }:
+{
+  programs.nnn = {
+    enable = true;
+    package = pkgs.nnn.override ({ withNerdIcons = true; });
+#    bookmarks = {
+#      d = "$HOME/Downloads";
+#      ws = "$HOME/workspace";
+#    };
+    extraPackages = with pkgs; [
+      bat
+      eza
+      fzf
+      imv
+      mediainfo
+      ffmpegthumbnailer
+      zoxide
+    ];
+    plugins = {
+      mappings = {
+        j = "autojump";
+        v = "imgview";
+	i = "mediainfo";
+      };
+    };
+  };
+  programs.zsh.initExtra = lib.optionalString config.programs.zsh.enable ''
+    # Export NNN_TMPFILE to quit on cd always
+    export NNN_TMPFILE="${config.xdg.configHome}/nnn/.lastd"
+    . ${config.programs.nnn.finalPackage}/share/quitcd/quitcd.bash_sh_zsh
+  '';
+}
