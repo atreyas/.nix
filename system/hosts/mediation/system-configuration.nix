abe12f48f94582bde0cca84184f54861d4bca11c
Atreya Srivathsan <1410778+atreyas@users.noreply.github.com>
2026-03-22 19:04:52 +0000

    Fix mediation configs

--- system/hosts/mediation/system-configuration.nix
+++ system/hosts/mediation/system-configuration.nix
@@ -1,8 +1,10 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 {
   boot = {
     initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod" ];
     kernelModules = [ "kvm-intel" ];
+    kernelPackages = lib.mkForce pkgs.linuxPackages;
+    initrd.luks.devices."root".device = "/dev/disk/by-uuid/9dbb05f9-fa9c-42b9-b660-004e2ffd9567";
   };
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
